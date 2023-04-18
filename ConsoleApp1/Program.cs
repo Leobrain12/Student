16e6d49a67e284d4aa3957230f04ef7f63ed639f
Leobrain12 <99803711+Leobrain12@users.noreply.github.com>
2023-04-18 23:14:24 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Human
+    {
+        public Human(string fio, int age)
+        {
+            this.Fio = fio;
+            this.Age = age;
+        }
+        public string Fio { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Фио: {Fio} Возраст: {Age}");
+        }
+    }
+
+        class Student : Human
+        {
+            public Student(string spec, int kurs, int group, string fio, int age) : base(fio, age)
+            {
+                this.Spec = spec;
+                this.Kurs = kurs;
+                this.Group = group;
+            }
+            public string Spec { get; set; }
+            public int Kurs { get; set; }
+            public int Group { get; set; }
+
+            public override void Display()
+            {
+                Console.WriteLine($"специальность: {Spec} Курс: {Kurs} Группа: {Group} ");
+            }
+        }
+    
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Human a = new Human("Daniil", 18);
+            Student b = new Student("ИСИП" , 3 ,520, "Daniil",18);
+            a.Display();
+            b.Display();
+
+        }
+    }
+}
